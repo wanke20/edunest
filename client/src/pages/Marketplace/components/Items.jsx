fec9a74abd5b4702b18e2cd0080fa5c37888125b
wanke20 <122842952+wanke20@users.noreply.github.com>
2023-09-02 14:33:45 -0400

    update marketplace page

--- client/src/pages/Marketplace/components/Items.jsx
+++ client/src/pages/Marketplace/components/Items.jsx
@@ -1,5 +1,4 @@
 import './components.scss';
-import logo from "../notebook.jpeg";
 import {
     SafeAreaView,
     FlatList,
@@ -8,13 +7,13 @@ import {
 } from 'react-native';
 
 let shoppingItems = [
-    { name: "Pencil", description: "Ticonderoga No. 2 Pencil", image: "" },
-    { name: "Pen", description: "Staples Ballpoint Pen", image: "" },
-    { name: "Paper", description: "W.B. Mason A2 Paper", image: "" },
-    { name: "Pencil Case", description: "Staples Black Pencil Case", image: "" },
-    { name: "Notebook", description: "5 Star Notebook", image: "" },
-    { name: "Calculator", description: "Ti-84 Scientific Caclulator", image: "../casio-calc.jpg" },
-    { name: "Folder", description: "5 Star Folder", image: "" },
+    { name: "Pencil", description: "Ticonderoga No. 2 Pencil", image: "/static/media/notebook-pic.0c6cd8f7ac12bbf563af.jpeg" },
+    { name: "Pen", description: "Staples Ballpoint Pen", image: "/static/media/notebook-pic.0c6cd8f7ac12bbf563af.jpeg" },
+    { name: "Paper", description: "W.B. Mason A2 Paper", image: "/static/media/notebook.2d57e16420e17d301c31.jpeg" },
+    { name: "Pencil Case", description: "Staples Black Pencil Case", image: "/static/media/notebook.2d57e16420e17d301c31.jpeg" },
+    { name: "Notebook", description: "5 Star Notebook", image: "/static/media/notebook.2d57e16420e17d301c31.jpeg" },
+    { name: "Calculator", description: "Ti-84 Scientific Caclulator", image: "/static/media/notebook.2d57e16420e17d301c31.jpeg" },
+    { name: "Folder", description: "5 Star Folder", image: "/static/media/notebook.2d57e16420e17d301c31.jpeg" },
 ];
 
 const screenWidth = Dimensions.get('window').width;
@@ -26,8 +25,8 @@ const App = () => {
         return (
             <div className='items-container'>
                 <button className='item'>
-                    <img src={image} width="100" height="50" className='image' />
-                    <Text className='title'>{title}<br /></Text>
+                    <img src={image} width={boxWidth - 4} height={boxWidth * 0.67} alt="" />
+                    <Text className='title-text'><br />{title}<br /></Text>
                     <Text className='normal-text'>{description}</Text>
                 </button>
             </div>
@@ -35,16 +34,17 @@ const App = () => {
     };
 
     return (
+        <div>
+            <SafeAreaView className="items-container">
+                <FlatList
+                    data={shoppingItems}
+                    renderItem={({ item }) => <Item title={item.name} description={item.description} image={item.image} />}
+                    keyExtractor={item => item.id}
+                    numColumns={4}
 
-        <SafeAreaView className="items-container">
-            <FlatList
-                data={shoppingItems}
-                renderItem={({ item }) => <Item title={item.name} description={item.description} image={item.image} />}
-                keyExtractor={item => item.id}
-                numColumns={4}
-
-            />
-        </SafeAreaView>
+                />
+            </SafeAreaView>
+        </div>
     );
 };
 
