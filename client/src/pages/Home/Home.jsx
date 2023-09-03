a45ccf97b2aeabd5d08565bd56a8ad399533b61d
debsouryadatta <debsouryadatta@gmail.com>
2023-09-03 10:20:39 +0530

    Dashboard frontend added

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import './Home.scss'
+import { Link } from 'react-router-dom';
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <>
+      <div className='card-container'>
+        <Link to={'/marketplace'} className='card'>
+          Marketplace
+        </Link>
+        <Link to={'/profile'} className='card'>
+          Profile
+        </Link>
+      </div>
+
+
+    </>
   )
 }
 
