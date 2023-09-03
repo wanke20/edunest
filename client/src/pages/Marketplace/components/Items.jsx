0160e8de4910517ea4c1fa86d814efb17f01ef36
wanke20 <122842952+wanke20@users.noreply.github.com>
2023-09-02 22:27:42 -0400

    update Items.jsx

--- client/src/pages/Marketplace/components/Items.jsx
+++ client/src/pages/Marketplace/components/Items.jsx
@@ -6,25 +6,28 @@ import {
 } from 'react-native';
 
 let shoppingItems = [
-    { name: "Ticonderoga No. 2 Pencil", image: require('../notebook.jpeg') },
-    { name: "Staples Ballpoint Pen", image: require('../notebook.jpeg') },
-    { name: "W.B. Mason A2 Paper", image: require('../notebook.jpeg') },
-    { name: "Staples Black Pencil Case", image: require('../notebook.jpeg') },
-    { name: "5 Star Notebook", image: require('../notebook.jpeg') },
-    { name: "Ti-84 Scientific Caclulator", image: require('../notebook.jpeg') },
-    { name: "5 Star Folder", image: require('../notebook.jpeg') },
+    { name: "Ticonderoga No. 2 Pencil", tags: ['pencil'], image: require('../notebook.jpeg') },
+    { name: "Staples Ballpoint Pen", tags: ['pen', 'staples'], image: require('../notebook.jpeg') },
+    { name: "W.B. Mason A2 Paper", tags: ['paper'], image: require('../notebook.jpeg') },
+    { name: "Staples Black Pencil Case", tags: ['case', 'pencil', 'staples'], image: require('../notebook.jpeg') },
+    { name: "5 Star Notebook", tags: ['notebook', '5 star'], image: require('../notebook.jpeg') },
+    { name: "Ti-84 Scientific Caclulator", tags: ['calculator', 'scientific'], image: require('../notebook.jpeg') },
+    { name: "5 Star Folder", tags: ['folder', '5 star'], image: require('../notebook.jpeg') },
 ];
 
 const screenWidth = Dimensions.get('window').width;
 const boxWidth = (screenWidth - 75) / 4;
-document.documentElement.style.setProperty("--background-width", `${boxWidth}px`);
+const root = document.documentElement;
+root?.style.setProperty("--background-width", `${boxWidth}px`);
+root?.style.setProperty("--image-width", `${boxWidth - 4}px`);
+root?.style.setProperty("--image-height", `${boxWidth * 2 / 3}px`);
 
 const App = () => {
     const Item = ({ title, image }) => {
         return (
             <div className='components'>
                 <button className='item'>
-                    <img src={image} width={boxWidth - 4} height={boxWidth * 2 / 3} alt="" />
+                    <img src={image} alt="" />
                     <h1 className="normal-text"><br />{title}<br /></h1>
                 </button>
             </div>
