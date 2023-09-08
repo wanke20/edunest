77c5985805172fcc933db50d1233cceaa659a537
avinasdube <avidubey712@gamil.com>
2023-09-08 22:17:45 +0530

    [fix]: workflow fix

--- scripts/auto-label.js
+++ scripts/auto-label.js
@@ -1,12 +1,12 @@
-import { getInput, setFailed } from '@actions/core';
-import { getOctokit, context } from '@actions/github';
+const core = require('@actions/core');
+const github = require('@actions/github');
 
 async function run() {
   try {
-    const token = getInput('github-token', { required: true });
-    const octokit = getOctokit(token);
+    const token = core.getInput('github-token', { required: true });
+    const octokit = github.getOctokit(token);
 
-    const issue = context.payload.issue;
+    const issue = github.context.payload.issue;
     const issueTitle = issue.title.toLowerCase();
     const labels = [];
 
@@ -28,14 +28,14 @@ async function run() {
 
     if (labels.length > 0) {
       await octokit.rest.issues.addLabels({
-        owner: context.repo.owner,
-        repo: context.repo.repo,
+        owner: github.context.repo.owner,
+        repo: github.context.repo.repo,
         issue_number: issue.number,
         labels: labels
       });
     }
   } catch (error) {
-    setFailed(error.message);
+    core.setFailed(error.message);
   }
 }
 
